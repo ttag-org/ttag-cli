a20148d84ca33ff54e42137d780aa64a33860ca0
Alexander Mostovenko <alexmost1989@gmail.com>
2017-11-18 21:02:28 +0200

    progress display

--- src/commands/extract.ts
+++ src/commands/extract.ts
@@ -4,34 +4,51 @@ import * as fs from "fs";
 import * as walk from "walk";
 import * as path from "path";
 import { getPluralFormsHeader } from "plural-forms";
-
-function extractFile(filepath: string, babelOpts: babel.TransformOptions) {
-    const extname = path.extname(filepath);
-    if (extname === ".js" || extname === ".jsx") {
-        babel.transformFileSync(filepath, babelOpts);
-    }
-}
-
-async function extractDir(dirpath: string, babelOpts: babel.TransformOptions) {
-    const walker = walk.walk(dirpath);
-    walker.on("file", (root: string, fileState: any, next: any) => {
-        extractFile(path.join(root, fileState.name), babelOpts);
-        next();
-    });
-    return new Promise(res => {
-        walker.on("end", () => {
-            res();
-        });
-    });
-}
+import * as ora from "ora";
 
 type C3POOpts = {
     extract: Object;
     defaultHeaders?: Object;
 };
 
+type Progress = {
+    text: string;
+    start(text?: string | undefined): any;
+    succeed(text?: string | undefined): any;
+};
+
+function extractFile(
+    filepath: string,
+    babelOpts: babel.TransformOptions,
+    progress: Progress
+) {
+    const extname = path.extname(filepath);
+    if (extname === ".js" || extname === ".jsx") {
+        progress.text = filepath;
+        babel.transformFileSync(filepath, babelOpts);
+    }
+}
+
+async function extractDir(
+    dirpath: string,
+    babelOpts: babel.TransformOptions,
+    progress: Progress
+) {
+    const walker = walk.walk(dirpath);
+    walker.on("file", (root: string, fileState: any, next: any) => {
+        extractFile(path.join(root, fileState.name), babelOpts, progress);
+        next();
+    });
+    return new Promise(res => {
+        walker.on("end", () => res());
+    });
+}
+
 async function extract(output: string, paths: string[], locale: string = "en") {
-    console.log(`[c-3po] started extraction from ${paths} to ${output} ...`);
+    const progress: Progress = ora(
+        `[c-3po] extracting translations to ${output} ...`
+    );
+    progress.start();
     let c3pOptions: C3POOpts = { extract: { output } };
     if (locale !== "en") {
         const pluralHeaders = getPluralFormsHeader(locale);
@@ -45,14 +62,14 @@ async function extract(output: string, paths: string[], locale: string = "en") {
     await Promise.all(
         paths.map(async filePath => {
             if (fs.lstatSync(filePath).isDirectory()) {
-                await extractDir(filePath, babelOptions);
+                await extractDir(filePath, babelOptions, progress);
             } else {
-                extractFile(filePath, babelOptions);
+                extractFile(filePath, babelOptions, progress);
             }
         })
     );
 
-    console.log(`[c-3po] has successfully extracted translations to ${output}`);
+    progress.succeed(`[c-3po] translations extracted to ${output}`);
 }
 
 export default extract;
