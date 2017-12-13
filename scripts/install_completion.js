4b55176a80a2655561936be92a10d02399d3a0bb
v.haritonskiy <v.haritonskiy@smartweb.com.ua>
2017-12-13 19:29:43 +0200

    add postinstall completion

---
+++ scripts/install_completion.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+var fs = require('fs');
+var path = require('path');
+var completionDir = '/etc/bash_completion.d/';
+if (fs.existsSync(completionDir)) {
+    try {
+        fs.copyFileSync(path.join(__dirname, 'c-3po'), path.join(completionDir, 'c-3po'));
+    }catch(e) {
+        console.warn("Could not install bash completion, run install with --unsafe");
+    }
+} else {
+    console.log("Platform does not have completion feature or " + completionDir)
+}
