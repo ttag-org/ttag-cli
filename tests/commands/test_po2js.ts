b0c25e1ca6352ab7b599715d1fcda24714bc08ba
a.mostovenko <a.mostovenko@smartweb.com.ua>
2018-08-17 14:43:33 +0300

    renamed po2js -> po2json

--- tests/commands/test_po2js.ts
+++ tests/commands/test_po2js.ts
@@ -4,20 +4,22 @@ import { execSync } from "child_process";
 const poPath = path.resolve(__dirname, "../fixtures/po2jsTest/po2js.po");
 
 test("convert po to js", () => {
-    const result = execSync(`ts-node src/index.ts po2js ${poPath}`).toString();
+    const result = execSync(
+        `ts-node src/index.ts po2json ${poPath}`
+    ).toString();
     expect(result).toMatchSnapshot();
 });
 
 test("convert po to js pretty", () => {
     const result = execSync(
-        `ts-node src/index.ts po2js -p ${poPath}`
+        `ts-node src/index.ts po2json -p ${poPath}`
     ).toString();
     expect(result).toMatchSnapshot();
 });
 
 test("convert po to js nostrip", () => {
     const result = execSync(
-        `ts-node src/index.ts po2js -n ${poPath}`
+        `ts-node src/index.ts po2json -n ${poPath}`
     ).toString();
     expect(result).toMatchSnapshot();
 });
