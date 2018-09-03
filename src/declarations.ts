9279ebef875086156dfed33ae141227d9bdbd7cd
a.mostovenko <a.mostovenko@smartweb.com.ua>
2018-09-03 18:36:54 +0300

    fix babel-core import

--- src/declarations.ts
+++ src/declarations.ts
@@ -4,5 +4,4 @@ declare module "hunspell-spellchecker";
 declare module "gettext-parser";
 declare module "gettext-parser/lib/shared";
 declare module "babel-plugin-ttag";
-declare module "babel-core";
 declare module "@babel/preset-react";
