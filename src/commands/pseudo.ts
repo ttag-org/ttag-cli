0c86db2210d9f5695d19483df60f4cb87e6217db
Dima Tisnek <dimaqq@gmail.com>
2019-03-11 20:28:51 +0900

    wip: Pseudolocalize using Mathematics Symbols

--- src/commands/pseudo.ts
+++ src/commands/pseudo.ts
@@ -2,6 +2,33 @@ import * as fs from "fs";
 import { iterateTranslations } from "../lib/utils";
 import { parse } from "../lib/parser";
 import { printHeader, printMsg } from "../lib/print";
+import { ast2Str } from "../lib/utils";
+import { ExpressionStatement, TemplateLiteral } from "@babel/types";
+import tpl from "@babel/template";
+
+const UPPER_A = <number>"A".codePointAt(0);
+const UPPER_Z = <number>"Z".codePointAt(0);
+const LOWER_A = <number>"a".codePointAt(0);
+const LOWER_Z = <number>"z".codePointAt(0);
+
+const PSEUDO_UPPER_A = 0x1d608;
+const PSEUDO_LOWER_A = 0x1d622;
+
+function pseudoChar(c: string) {
+    let code = <number>c.codePointAt(0);
+    if (code >= UPPER_A && code <= UPPER_Z) {
+        code = code - UPPER_A + PSEUDO_UPPER_A;
+    } else if (code >= LOWER_A && code <= LOWER_Z) {
+        code = code - LOWER_A + PSEUDO_LOWER_A;
+    }
+    return String.fromCodePoint(code);
+}
+
+function pseudoLocalise(str: string) {
+    return Array.from(str)
+        .map(pseudoChar)
+        .join("");
+}
 
 export default function pseudo(path: string) {
     const poData = parse(fs.readFileSync(path).toString());
@@ -15,6 +42,19 @@ export default function pseudo(path: string) {
     for (const msg of messages) {
         printMsg(msg);
         process.stdout.write("\n");
+        const msgid: string = msg.msgid;
+        const statement = <ExpressionStatement>tpl("`" + msgid + "`")();
+        const expression = <TemplateLiteral>statement.expression;
+
+        for (const q of expression.quasis) {
+            if (q.value.raw) {
+                q.value.raw = pseudoLocalise(q.value.raw);
+            }
+            if (q.value.cooked) {
+                q.value.cooked = pseudoLocalise(q.value.cooked);
+            }
+        }
+        process.stdout.write("Z: " + ast2Str(expression) + "\n");
     }
 
     /*
